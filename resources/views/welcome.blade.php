e90d6664547ea8bde1895624c1cd1f3faf0ee8c9
Marino Ricardo <marino814@bitbucket.org>
2021-09-25 01:45:19 -0700

    melhorando as rotas para crud

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                {{$nome}}
+                Curso de LAravel
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,55 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::view('/teste', 'welcome', ['nome' => 'Curso de Laravel 6']);
+
+Route::get('/login', function(){
+    return "Login";
+})->name('login');
+
+// Route::middleware([])->group(function (){
+
+//     // podemos tambem definir o prefixo
+//     Route::prefix('admin')->group(function (){
+
+//         Route::namespace('Admin')->group(function () {
+//             Route::name('admin.')->group(function (){
+//                 Route::get('/dashboard', 'TesteController@teste')->name('dashboard'); // middleware serve para limitar acesse nessa rota
+//                 Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+//                 Route::get('/produtos', 'TesteController@teste')->name('produtos');
+//                 Route::get('/', 'TesteController@teste')->name('home');
+//             });
+
+//         });
+        
+//     });
+
+// });
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+    'name' => 'admin.'
+], function(){
+    Route::get('/dashboard', 'TesteController@teste')->name('dashboard'); // middleware serve para limitar acesso
+    Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+    Route::get('/produtos', 'TesteController@index')->name('produtos');
+    Route::get('/', 'TesteController@teste')->name('home');
+
+});
+
+
+// Route::delete('products/{id}', 'ProductController@destroy')->name('products.destroy');
+// Route::put('products/{id}', 'ProductController@update')->name('products.update');
+// Route::get('products/{id}/edit', 'ProductController@edit')->name('products.edit');
+// Route::get('products/create', 'ProductController@create')->name('products.create');
+// Route::get('products/{id}', 'ProductController@show')->name('products.show');
+// Route::get('products', 'ProductController@index')->name('products.index');
+// Route::post('products', 'ProductController@store')->name('products.store');
+
+Route::resource('products', 'ProductController'); //->middleware('auth');
+
+
+// Route::view('/teste', 'welcome', ['nome' => 'Curso de Laravel 6']);
 
 Route::redirect('/redirect1','/redirect2');
 Route::get('redirect1', function(){
