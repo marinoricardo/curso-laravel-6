57fcd7c0acd959c1f981c4119e01934eccad1825
Marino Ricardo <marino814@bitbucket.org>
2021-09-25 00:14:49 -0700

    criando rotas

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::view('/teste', 'welcome', ['nome' => 'Curso de Laravel 6']);
+
+Route::redirect('/redirect1','/redirect2');
+Route::get('redirect1', function(){
+    return redirect('/redirect2');
+});
+Route::get('redirect2', function(){
+    return "Redirect 02";
+});
+
+Route::get('/contacto', function(){
+    return view('contacto');
+});
+Route::get('/categorias/{flag}', function($flag){
+    return "Productos da categoria: {$flag}";
+});
+
+Route::get('/any', function(){
+    return "Any";
+});
 
 Route::get('/', function () {
     return view('welcome');
